a2e37367e303948309a4fee6df7c30f9318070d1
mong_head <camara1melon315@gmail.com>
2021-05-20 11:02:34 +0900

    query(insert) test

---
+++ jdbc-practices/src/main/java/test/InsertTest01.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTest01 {
+
+	public static void main(String[] args) {
+		insertDepartment("영업");
+		insertDepartment("총무");
+	}
+	public static boolean insertDepartment(String name){
+		Connection conn = null;
+		Statement stmt = null;
+		//ResultSet rs = null;
+		boolean result = false;
+		try {
+			// 1. JDBC driver loading
+			// 		loading 후 Driver Class가 DriverManager만듦 - 우리가 new로 drivermanager만들 필요없음
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. DriverManager - get connection
+			String url = "jdbc:mysql://192.168.254.40:3307/employees?chareset=utf8"; //linux server ip
+			conn	   = DriverManager.getConnection(url,"hr","hr");
+			conn.createStatement();
+			System.out.println("success connection");
+			
+			// 3. Statement 생성 - query 실행위함
+			stmt = conn.createStatement();
+			
+			// 4. SQL실행
+			String sql = "insert into dept values(null,'"+ name +"');";
+			int count = stmt.executeUpdate(sql);
+			
+			result = count == 1;
+			
+		} catch (ClassNotFoundException e) {
+			// 1. 관련 : pom에 dependency로 jdbc driver 안해놓음
+			System.out.println("fail driver loading:"+e);
+		} catch (SQLException e) {
+			// 2. 관련 : linux 꺼져있을 때 등등 connection안될 때 
+			System.out.println("error :"+e);
+		} finally {
+			//clean-up; 자원정리는 만들어진 순서 거꾸로 하기
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+				if(conn != null) {
+					conn.close();					
+				}
+				
+			} catch (SQLException e) {
+				System.out.println("connection close error:"+e);
+			}
+		}
+		return result;
+	}
+}
