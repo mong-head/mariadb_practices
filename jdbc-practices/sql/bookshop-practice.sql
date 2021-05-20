f2573afec90fec63a1c5ef9e54931541afc352b3
mong_head <camara1melon315@gmail.com>
2021-05-20 17:13:18 +0900

    bookshop-practice, bookshop-model

---
+++ jdbc-practices/sql/bookshop-practice.sql
@@ -0,0 +1,18 @@
+use webdb;
+-- schema 확인 --
+desc author;
+desc book;
+
+-- Author: insert 
+-- insert into author values(null,'원수연');
+
+-- Author : select
+select no,name from author;
+
+select b.no,title,status,a.no, name
+from book b
+join author a on b.author_no = a.no;
+
+-- Book : insert
+-- insert into book values(null,'풀하우스','대여가능', 1);
+
